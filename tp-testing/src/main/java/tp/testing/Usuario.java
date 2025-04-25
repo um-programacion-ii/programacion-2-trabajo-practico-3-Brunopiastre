7f8560375670741e883ffd9cedc9ae189171bfce
Brunopiastre <b.piastrellini@alumno.um.edu.ar>
2025-04-25 19:27:46 -0300

    🔁 Milestone 3 completo – Sistema de préstamos con pruebas usando Mockito

---
+++ tp-testing/src/main/java/tp/testing/Usuario.java
@@ -0,0 +1,19 @@
+package tp.testing;
+
+public class Usuario {
+    private String nombre;
+    private String email;
+
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
